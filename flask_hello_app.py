2adbee999120519536301a519d7354dd92bb30a0
Majey <kevinerick78@yahoo.com>
2022-07-30 09:38:07 +0300

    SQL and Data Modeling for the Web

--- flask_hello_app.py
+++ flask_hello_app.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:ICUI4CU1997@localhost:5432/example"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
@@ -11,9 +10,6 @@ class Person(db.Model):
     __tablename__ = "person"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
-
-    def __repr__(self):
-        return f"<Person ID: {self.id} name: {self.name}>"
 
 db.create_all()
 
